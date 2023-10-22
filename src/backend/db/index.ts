eeafb312e0d1c1f4e352102b85acd2ee279ccafb
Tonipenyallop <kimtesu521@gmail.com>
2023-10-22 15:10:51 +0900

    use: dotenv

---
+++ src/backend/db/index.ts
@@ -0,0 +1,12 @@
+import { Pool } from "pg";
+import dotenv from "dotenv";
+// loading environment variables
+dotenv.config();
+
+export const pool = new Pool({
+  user: process.env.DB_USERNAME,
+  host: process.env.DB_HOSTNAME,
+  database: process.env.DB_DATABASE_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
+});
