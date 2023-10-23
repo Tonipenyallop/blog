cf1139e86ed8eaafdfbd1e4bdf2402298ab74e0f
Tonipenyallop <kimtesu521@gmail.com>
2023-10-23 22:19:14 +0900

    add: create component

---
+++ src/create/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Button, Form, InputGroup } from "react-bootstrap";
+
+interface Props {
+  context: string;
+  title: string;
+  sendRequest: (event: any) => void;
+  updateContext: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  updateTitle: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function Create({
+  context,
+  title,
+  sendRequest,
+  updateContext,
+  updateTitle,
+}: Props) {
+  return (
+    <div>
+      <InputGroup>
+        <InputGroup.Text id="basic-addon1">Title</InputGroup.Text>
+        <Form.Control
+          placeholder="Username"
+          aria-label="Username"
+          aria-describedby="basic-addon1"
+          onChange={updateTitle}
+          value={title}
+        />
+      </InputGroup>
+      <InputGroup>
+        <Form.Control
+          rows={50}
+          as="textarea"
+          aria-label="With textarea"
+          onChange={updateContext}
+          placeholder="description..."
+          value={context}
+        />
+      </InputGroup>
+      <Button disabled={context === "" || title === ""} onClick={sendRequest}>
+        submit
+      </Button>
+    </div>
+  );
+}
