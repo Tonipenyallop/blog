1ae6b756e8bbda342507cf8dddc1f85c6e44a0a9
Tonipenyallop <kimtesu521@gmail.com>
2023-12-14 22:21:32 +0900

    update: null handler

--- src/backend/services/users/index.ts
+++ src/backend/services/users/index.ts
@@ -2,6 +2,10 @@ import { userRepository } from "../../repositories/users/index";
 import { User } from "../../../types";
 export const userService = {
   signUp: (user: User) => {
+    const { username, password } = user;
+    if (!username || !password) {
+      return null;
+    }
     return userRepository.signUp(user);
   },
 };
