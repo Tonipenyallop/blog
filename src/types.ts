8661c35745852cdbfc5beba7514ec715f65d3fbe
Tonipenyallop <kimtesu521@gmail.com>
2023-12-14 22:20:49 +0900

    add: type

--- src/types.ts
+++ src/types.ts
@@ -5,3 +5,28 @@ export interface Post {
   context: string;
   created_at: string;
 }
+
+export interface User {
+  id: number;
+  username: string;
+  currentChallenge?: string;
+  password: string;
+  authenticatorID?: number;
+}
+
+export interface Authenticator {
+  // SQL: Encode to base64url then store as `TEXT`. Index this column
+  credentialID: Uint8Array;
+  // SQL: Store raw bytes as `BYTEA`/`BLOB`/etc...
+  credentialPublicKey: Uint8Array;
+  // SQL: Consider `BIGINT` since some authenticators return atomic timestamps as counters
+  counter: number;
+  // SQL: `VARCHAR(32)` or similar, longest possible value is currently 12 characters
+  // Ex: 'singleDevice' | 'multiDevice'
+  credentialDeviceType: string;
+  // SQL: `BOOL` or whatever similar type is supported
+  credentialBackedUp: boolean;
+  // SQL: `VARCHAR(255)` and store string array as a CSV string
+  // Ex: ['usb' | 'ble' | 'nfc' | 'internal']
+  transports?: AuthenticatorTransport[];
+}
