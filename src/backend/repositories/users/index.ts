cacb25e00edf7fbaf71cf9b42cec50740e39a85d
Tonipenyallop <kimtesu521@gmail.com>
2023-12-20 23:22:09 +0900

    update: user repository

--- src/backend/repositories/users/index.ts
+++ src/backend/repositories/users/index.ts
@@ -5,22 +5,29 @@ import bcrypt from "bcrypt";
 const UserEntity = AppDataSource.getRepository(User);
 
 export const userRepository = {
-  signUp: (user: User) => {
-    const { username, password, email } = user;
-    if (!username || !password || !email) {
-      throw new Error(`username or password should be provided`);
-    }
-    const salt = bcrypt.genSaltSync();
-    const hashedPassword = bcrypt.hashSync(password, salt);
+  createUser: async (user: User) => {
     try {
+      const { username, password, email } = user;
+      if (!username || !password || !email) {
+        throw new Error(`username or password should be provided`);
+      }
+
+      const userAlreadyExist = await userRepository.getUserByEmail(email);
+      if (userAlreadyExist) {
+        throw new Error(`user already exists`);
+      }
+
+      const salt = bcrypt.genSaltSync();
+      const hashedPassword = bcrypt.hashSync(password, salt);
       const newUser = UserEntity.create({
         username,
         password: hashedPassword,
         email,
       });
+
       return UserEntity.save(newUser);
     } catch (err) {
-      throw new Error(`error occurred while sending SELECT schema: ${err}`);
+      throw new Error(`error occurred while creating user: ${err}`);
     }
   },
   getUserByEmail: (email: string) => {
@@ -30,7 +37,34 @@ export const userRepository = {
     try {
       return UserEntity.findOneBy({ email });
     } catch (err) {
-      throw new Error(`error occurred while sending SELECT schema: ${err}`);
+      throw new Error(`error occurred while getting user by email: ${err}`);
+    }
+  },
+  getUserByID: (userID: number) => {
+    try {
+      console.log("getUserByID was called mate");
+      console.log(typeof userID);
+      console.log(userID);
+      if (typeof userID !== "number") {
+        throw new Error(`userID should be provided`);
+      }
+      return UserEntity.findOneBy({ id: userID });
+    } catch (err) {
+      throw new Error(`error occurred while getting user by ID: ${err}`);
+    }
+  },
+
+  setUserCurrentChallenge(userID: number, challenge: string) {
+    if (typeof userID !== "number" || !challenge) {
+      throw new Error(`user or challenge should be provided`);
+    }
+    try {
+      return UserEntity.update(
+        { id: userID },
+        { current_challenge: challenge }
+      );
+    } catch (err) {
+      throw new Error(`error occurred while setting challenge: ${err}`);
     }
   },
 };
