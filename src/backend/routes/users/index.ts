add1cf32f079f52afe92c9f14d9906365687f2f4
Tonipenyallop <kimtesu521@gmail.com>
2023-12-20 23:25:30 +0900

    add: verifyRegistration

--- src/backend/routes/users/index.ts
+++ src/backend/routes/users/index.ts
@@ -1,39 +1,36 @@
 import express, { Request, Response } from "express";
 import { userService } from "../../services/users/index";
-import { authService } from "../../services/auth/index";
+import { AuthService } from "../../services/auth/index";
 import { User } from "../../../types";
-import dotenv from "dotenv";
-import {
-  generateRegistrationOptions,
-  verifyRegistrationResponse,
-} from "@simplewebauthn/server";
-
-dotenv.config();
-
-const isProduction = process.env.REACT_APP_IS_PRODUCTION ?? false;
-
-// Human-readable title for your website
-const rpName = "Toni blog";
-// A unique identifier for your website
-const rpID = isProduction ? process.env.PRODUCTION_DOMAIN : "localhost";
-// The URL at which registrations and authentications should occur
-const origin = `https://${rpID}`;
 
 const UserRouter = express.Router();
 
 UserRouter.post("/sign-up", async (req: Request<User>, res: Response) => {
   try {
     const user = req.body as User;
+
     const userData = await userService.signUp(user);
     if (userData === null) {
       return res.sendStatus(400);
     }
-    // authenticate here
+
     return res.status(201).send(userData);
   } catch (err) {
-    console.error(`err while getting posts: ${err}`);
-    throw err;
+    return res.status(409).send({ err });
   }
 });
+
+UserRouter.post(
+  "/verify-registration",
+  async (req: Request<User>, res: Response) => {
+    const { authenticatorResponse, userID } = req.body;
+    try {
+      return AuthService.verifyRegistration(authenticatorResponse, userID);
+    } catch (err) {
+      console.error(`err while verifying registration: ${err}`);
+      throw err;
+    }
+  }
+);
 
 export default UserRouter;
