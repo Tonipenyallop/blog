172064bad3de3672d09f8d06c078582d8a922122
Tonipenyallop <kimtesu521@gmail.com>
2023-12-20 23:27:31 +0900

    add: option to signUp method

--- src/backend/services/users/index.ts
+++ src/backend/services/users/index.ts
@@ -1,11 +1,22 @@
 import { userRepository } from "../../repositories/users/index";
+import { AuthService } from "../../services/auth";
 import { User } from "../../../types";
 export const userService = {
-  signUp: (user: User) => {
-    const { username, password, email } = user;
-    if (!username || !password || !email) {
-      return null;
+  signUp: async (user: User) => {
+    try {
+      console.log("signUp was called");
+      const { username, password, email } = user;
+      console.log("username, password, email");
+      console.log(username, password, email);
+      if (!username || !password || !email) {
+        return null;
+      }
+      await userRepository.createUser(user);
+      console.log("can you see me");
+      const option = await AuthService.createRegistrationOption(email);
+      return option;
+    } catch (err) {
+      throw new Error(`error while signing up ${err}`);
     }
-    return userRepository.signUp(user);
   },
 };
