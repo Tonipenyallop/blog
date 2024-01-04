414839757cfdec902c01824d6e4f48587bbde4ca
Tonipenyallop <kimtesu521@gmail.com>
2024-01-04 21:59:28 +0900

    update: user service

--- src/backend/services/users/index.ts
+++ src/backend/services/users/index.ts
@@ -17,4 +17,28 @@ export const userService = {
       throw new Error(`error while signing up ${err}`);
     }
   },
+  login: async (password: string, email: string) => {
+    try {
+      if (!password || !email) {
+        return null;
+      }
+      return userRepository.compareUserPassword(password, email);
+    } catch (err) {
+      throw new Error(`error while logging in ${err}`);
+    }
+  },
+  setAuthenticatorID: async (email: string, authenticatorRawID: string) => {
+    if (!email || !authenticatorRawID) {
+      return null;
+    }
+    try {
+      const user = await userRepository.getUserByEmail(email);
+      if (!user) {
+        return null;
+      }
+      return userRepository.setAuthenticatorID(user.id, authenticatorRawID);
+    } catch (err) {
+      throw new Error("error while setting authenticator ID");
+    }
+  },
 };
