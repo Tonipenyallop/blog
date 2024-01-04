9a441731ddd5a41db508feb789f5eed949cf3046
Tonipenyallop <kimtesu521@gmail.com>
2024-01-04 21:57:16 +0900

    update: user route

--- src/backend/routes/users/index.ts
+++ src/backend/routes/users/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import { userService } from "../../services/users/index";
 import { AuthService } from "../../services/auth/index";
 import { User } from "../../../types";
+import { PublicKeyCredentialRequestOptionsJSON } from "@simplewebauthn/server/script/deps";
 
 const UserRouter = express.Router();
 
@@ -32,6 +33,69 @@ UserRouter.post(
       return res.status(200).send(verified);
     } catch (err) {
       console.error(`err while verifying registration: ${err}`);
+      throw err;
+    }
+  }
+);
+
+UserRouter.post("/login", async (req: Request<User>, res: Response) => {
+  const { password, email } = req.body;
+  try {
+    const loginStatus = await userService.login(password, email);
+    return res.status(200).send({ loginStatus });
+  } catch (err) {
+    console.error(`err while logging in : ${err}`);
+    throw err;
+  }
+});
+
+UserRouter.post(
+  "/generate-authentication-options",
+  async (req: Request<User>, res: Response) => {
+    const { email } = req.body;
+    try {
+      const options = await AuthService.generateAuthenticationOptions(email);
+      return res.status(200).send({ options });
+    } catch (err) {
+      console.error(`err while logging in : ${err}`);
+      throw err;
+    }
+  }
+);
+
+UserRouter.post(
+  "/verify-authentication",
+  async (
+    req: Request<PublicKeyCredentialRequestOptionsJSON>,
+    res: Response
+  ) => {
+    console.log("verify-authentication");
+    const { email, authenticationResponse } = req.body;
+    try {
+      const verification = await AuthService.verifyAuthentication(
+        email,
+        authenticationResponse
+      );
+      return res.status(200).send({ verification });
+    } catch (err) {
+      console.error(`err while logging in : ${err}`);
+      throw err;
+    }
+  }
+);
+
+UserRouter.post(
+  "/authenticate",
+  async (req: Request<string>, res: Response) => {
+    const { email, authenticatorRawID } = req.body;
+    try {
+      // store to auth raw id to user and auth collections
+      await userService.setAuthenticatorID(email, authenticatorRawID);
+      await AuthService.setRawID(email, authenticatorRawID);
+
+      return res.sendStatus(200);
+    } catch (err) {
+      console.error(`err while logging in : ${err}`);
       throw err;
     }
   }
