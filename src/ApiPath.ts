0ef0be345ab4e56c227a770903f137222a97f66a
Tonipenyallop <kimtesu521@gmail.com>
2024-01-16 19:21:24 +0900

    add: test console log

--- src/ApiPath.ts
+++ src/ApiPath.ts
@@ -1,8 +1,11 @@
 import dotenv from "dotenv";
 
 dotenv.config();
-
+console.log("process.env.REACT_APP_IS_PRODUCTION");
+console.log(process.env.REACT_APP_IS_PRODUCTION);
 const isProduction = process.env.REACT_APP_IS_PRODUCTION ?? false;
+console.log("isProduction");
+console.log(isProduction);
 
 const ApiPath = isProduction
   ? process.env.PRODUCTION_API_DOMAIN
