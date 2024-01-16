43ccc1632e106939913ad80977d5723400bdb2f3
Tonipenyallop <kimtesu521@gmail.com>
2024-01-16 20:07:03 +0900

    remove: print statement

--- src/ApiPath.ts
+++ src/ApiPath.ts
@@ -3,16 +3,10 @@ import { Environment } from "./types";
 
 dotenv.config();
 
-console.log("process.env.NODE_ENV");
-console.log(process.env.NODE_ENV);
-console.log("Environment.PRODUCTION");
-console.log(Environment.PRODUCTION);
-export const isProduction = process.env.NODE_ENV === Environment.PRODUCTION;
+const isProduction = process.env.NODE_ENV === Environment.PRODUCTION;
 
 const ApiPath = isProduction
   ? process.env.REACT_APP_PRODUCTION_API_DOMAIN
   : "http://localhost:3001";
-console.log("ApiPath");
-console.log(ApiPath);
 
 export default ApiPath;
