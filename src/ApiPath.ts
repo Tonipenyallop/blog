ea6b75ef3bdd5645c2bdae540c587daa86acb53d
Tonipenyallop <kimtesu521@gmail.com>
2024-01-16 19:38:20 +0900

    update: isProduction logic

--- src/ApiPath.ts
+++ src/ApiPath.ts
@@ -1,16 +1,15 @@
 import dotenv from "dotenv";
+import { Environment } from "./types";
 
 dotenv.config();
-console.log("process.env.NODE_ENV");
-console.log(process.env.NODE_ENV);
-console.log("process.env.REACT_APP_IS_PRODUCTION");
-console.log(process.env.REACT_APP_IS_PRODUCTION);
-const isProduction = process.env.REACT_APP_IS_PRODUCTION ?? false;
-console.log("isProduction");
-console.log(isProduction);
+
+export const isProduction =
+  process.env.REACT_APP_IS_PRODUCTION === Environment.PRODUCTION;
 
 const ApiPath = isProduction
   ? process.env.PRODUCTION_API_DOMAIN
   : "http://localhost:3001";
+console.log("ApiPath");
+console.log(ApiPath);
 
 export default ApiPath;
